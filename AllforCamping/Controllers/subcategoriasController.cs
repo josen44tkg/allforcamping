6f4f5cb5c794a9266274075cc42f26a7d4c62f35
Cinthia Villegas Soto <cvillegass@toluca.tecnm.mx>
2021-06-20 18:27:04 -0500

    Subcategorias controler

---
+++ AllforCamping/Controllers/subcategoriasController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AllforCamping.Models;
+
+namespace AllforCamping.Controllers
+{
+    public class subcategoriasController : Controller
+    {
+        private allforcampingEntities1 db = new allforcampingEntities1();
+
+        // GET: subcategorias
+        public ActionResult Index()
+        {
+            var subcategorias = db.subcategorias.Include(s => s.categorias);
+            return View(subcategorias.ToList());
+        }
+
+        // GET: subcategorias/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            subcategorias subcategorias = db.subcategorias.Find(id);
+            if (subcategorias == null)
+            {
+                return HttpNotFound();
+            }
+            return View(subcategorias);
+        }
+
+        // GET: subcategorias/Create
+        public ActionResult Create()
+        {
+            ViewBag.Id = new SelectList(db.categorias, "Id", "nombre");
+            return View();
+        }
+
+        // POST: subcategorias/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,nombre,categoria")] subcategorias subcategorias)
+        {
+            if (ModelState.IsValid)
+            {
+                db.subcategorias.Add(subcategorias);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Id = new SelectList(db.categorias, "Id", "nombre", subcategorias.Id);
+            return View(subcategorias);
+        }
+
+        // GET: subcategorias/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            subcategorias subcategorias = db.subcategorias.Find(id);
+            if (subcategorias == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Id = new SelectList(db.categorias, "Id", "nombre", subcategorias.Id);
+            return View(subcategorias);
+        }
+
+        // POST: subcategorias/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,nombre,categoria")] subcategorias subcategorias)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(subcategorias).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Id = new SelectList(db.categorias, "Id", "nombre", subcategorias.Id);
+            return View(subcategorias);
+        }
+
+        // GET: subcategorias/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            subcategorias subcategorias = db.subcategorias.Find(id);
+            if (subcategorias == null)
+            {
+                return HttpNotFound();
+            }
+            return View(subcategorias);
+        }
+
+        // POST: subcategorias/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            subcategorias subcategorias = db.subcategorias.Find(id);
+            db.subcategorias.Remove(subcategorias);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
