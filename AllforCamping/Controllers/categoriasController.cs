07141d489e0176a2b20bcf97338f5be32287e3dc
Cinthia Villegas Soto <cvillegass@toluca.tecnm.mx>
2021-06-20 17:14:31 -0500

    Controlador de categorias

---
+++ AllforCamping/Controllers/categoriasController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AllforCamping.Models;
+
+namespace AllforCamping.Controllers
+{
+    public class categoriasController : Controller
+    {
+        private allforcampingEntities1 db = new allforcampingEntities1();
+
+        // GET: categorias
+        public ActionResult Index()
+        {
+            var categorias = db.categorias.Include(c => c.subcategorias);
+            return View(categorias.ToList());
+        }
+
+        // GET: categorias/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            categorias categorias = db.categorias.Find(id);
+            if (categorias == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categorias);
+        }
+
+        // GET: categorias/Create
+        public ActionResult Create()
+        {
+            ViewBag.Id = new SelectList(db.subcategorias, "Id", "nombre");
+            return View();
+        }
+
+        // POST: categorias/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,nombre")] categorias categorias)
+        {
+            if (ModelState.IsValid)
+            {
+                db.categorias.Add(categorias);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Id = new SelectList(db.subcategorias, "Id", "nombre", categorias.Id);
+            return View(categorias);
+        }
+
+        // GET: categorias/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            categorias categorias = db.categorias.Find(id);
+            if (categorias == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Id = new SelectList(db.subcategorias, "Id", "nombre", categorias.Id);
+            return View(categorias);
+        }
+
+        // POST: categorias/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,nombre")] categorias categorias)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(categorias).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Id = new SelectList(db.subcategorias, "Id", "nombre", categorias.Id);
+            return View(categorias);
+        }
+
+        // GET: categorias/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            categorias categorias = db.categorias.Find(id);
+            if (categorias == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categorias);
+        }
+
+        // POST: categorias/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            categorias categorias = db.categorias.Find(id);
+            db.categorias.Remove(categorias);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
