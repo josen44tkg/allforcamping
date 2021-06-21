39d30b64328e2762de8921736dc0e2205fdf3d1e
YeyaSalazar <idsa2316@gmail.com>
2021-06-21 07:56:18 -0500

    Paqueteria

---
+++ AllforCamping/Controllers/PaqueteriasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AllforCamping.Controllers
+{
+    public class PaqueteriasController : Controller
+    {
+        // GET: Paqueterias
+        public ActionResult Paqueteria()
+        {
+            return View();
+        }
+    }
+}
