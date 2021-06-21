8ea79c42efc60655400756f41e138a38abaa1dfb
Cinthia Villegas Soto <cvillegass@toluca.tecnm.mx>
2021-06-20 21:30:00 -0500

    Controlador prueba

---
+++ AllforCamping/Controllers/PruebaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AllforCamping.Controllers
+{
+    public class PruebaController : Controller
+    {
+        // GET: Prueba
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
