96c725e45cb1148c3b9ad1997710852080e5a747
YeyaSalazar <idsa2316@gmail.com>
2021-06-21 07:06:53 -0500

    catalogo

---
+++ AllforCamping/Controllers/CatálogoController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AllforCamping.Controllers
+{
+    public class CatálogoController : Controller
+    {
+        // GET: Catalogo
+        public ActionResult Catalogo()
+        {
+            return View();
+        }
+    }
