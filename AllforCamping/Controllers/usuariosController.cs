f3941f5090c6313730b7ec2c55ce91c7f6699560
RicardReyesF <48768228+RicardReyesF@users.noreply.github.com>
2021-06-20 23:01:09 -0500

    CRUD usuarios

---
+++ AllforCamping/Controllers/usuariosController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AllforCamping.Models;
+
+namespace AllforCamping.Controllers
+{
+    public class usuariosController : Controller
+    {
+        private Database1Entities1 db = new Database1Entities1();
+
+        // GET: usuarios
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.usuarios.ToListAsync());
+        }
+
+        // GET: usuarios/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            usuarios usuarios = await db.usuarios.FindAsync(id);
+            if (usuarios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuarios);
+        }
+
+        // GET: usuarios/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: usuarios/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,nombre,ap_pat,ap_mat,email,telefono,username,pass,calle,numext,colonia")] usuarios usuarios)
+        {
+            if (ModelState.IsValid)
+            {
+                db.usuarios.Add(usuarios);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(usuarios);
+        }
+
+        // GET: usuarios/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            usuarios usuarios = await db.usuarios.FindAsync(id);
+            if (usuarios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuarios);
+        }
+
+        // POST: usuarios/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,nombre,ap_pat,ap_mat,email,telefono,username,pass,calle,numext,colonia")] usuarios usuarios)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(usuarios).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(usuarios);
+        }
+
+        // GET: usuarios/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            usuarios usuarios = await db.usuarios.FindAsync(id);
+            if (usuarios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuarios);
+        }
+
+        // POST: usuarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            usuarios usuarios = await db.usuarios.FindAsync(id);
+            db.usuarios.Remove(usuarios);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
